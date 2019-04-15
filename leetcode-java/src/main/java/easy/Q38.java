9251acdf55d41c5749fe56e89c56907849e3c347
freshchen <961011595@qq.com>
2019-04-15 17:40:47 +0800

    [leetcode] 38

---
+++ leetcode-java/src/main/java/easy/Q38.java
@@ -0,0 +1,26 @@
+package easy;
+
+/**
+ * @anthor LingChen
+ * @create 4/15/2019 5:13 PM
+ * @Description
+ */
+public class Q38 {
+    public String countAndSay(int n) {
+        String result = "1";
+        if (n > 1) {
+            int num = 1;
+            for (int i = 2; i < n + 1; i++) {
+
+            }
+
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- leetcode-java/src/main/resources/leetcode-plugin/cn/[38]报数.java
+++ leetcode-java/src/main/resources/leetcode-plugin/cn/[38]报数.java
@@ -32,6 +32,6 @@
 
 class Solution {
     public String countAndSay(int n) {
-        
+        return "";
     }
 }
