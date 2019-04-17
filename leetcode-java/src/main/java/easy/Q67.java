06ccb3731343a5e52f62d30c1f046250a05a3214
freshchen <961011595@qq.com>
2019-04-17 09:56:43 +0800

    [leetcode] 67

---
+++ leetcode-java/src/main/java/easy/Q67.java
@@ -0,0 +1,19 @@
+package easy;
+
+/**
+ * @anthor LingChen
+ * @create 4/16/2019 5:25 PM
+ * @Description
+ */
+public class Q67 {
+    public String addBinary(String a, String b) {
+        char[] aChar = a.toCharArray();
+        char[] bChar = b.toCharArray();
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Q67 q67 = new Q67();
+        System.out.println(q67.addBinary("101","001"));
+    }
+}
--- leetcode-java/src/main/resources/leetcode-plugin/cn/[38]报数.java
+++ leetcode-java/src/main/resources/leetcode-plugin/cn/[38]报数.java
@@ -32,6 +32,24 @@
 
 class Solution {
     public String countAndSay(int n) {
-        return "";
+        if (n <= 1) {
+            return "1";
+        }
+        String count = countAndSay(n-1);
+        char target = count.charAt(0);
+        int countNum = 1;
+        StringBuffer result = new StringBuffer();
+        for(int i=1; i<count.length(); i++) {
+            char temp = count.charAt(i);
+            if (target == temp) {
+                countNum++;
+            } else {
+                result.append(countNum).append(target);
+                target = temp;
+                countNum = 1;
+            }
+        }
+        result.append(countNum).append(target);
+        return result.toString();
     }
 }
