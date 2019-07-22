4dd49636efdf6f597bad4393a97ad812dcd26e02
freshchen <961011595@qq.com>
2019-07-22 22:58:50 +0800

    [fresh] leetcode

---
+++ leetcode-java/src/main/java/ListNode.java
@@ -0,0 +1,14 @@
+/**
+ * @program: fresh-algorithms
+ * @Date: 2019/7/22 22:34
+ * @Author: Ling Chen
+ * @Description:
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ leetcode-java/src/main/java/Q2.java
@@ -0,0 +1,33 @@
+/**
+ * @program: fresh-algorithms
+ * @Date: 2019/7/22 22:34
+ * @Author: Ling Chen
+ * @Description:
+ */
+public class Q2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode listNode1 = l1;
+        ListNode listNode2 = l2;
+        ListNode current = result;
+        int carry = 0;
+        while (listNode1 != null || listNode2 != null) {
+            int num1 = (listNode1 != null) ? listNode1.val : 0;
+            int num2 = (listNode1 != null) ? listNode1.val : 0;
+            int sum = carry + num1 + num2;
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+            if (listNode1 !=null){
+                listNode1 = listNode2.next;
+            }
+            if (listNode2 !=null){
+                listNode2 = listNode2.next;
+            }
+        }
+        if (carry > 0){
+            current.next = new ListNode(carry);
+        }
+        return result.next;
+    }
+}
