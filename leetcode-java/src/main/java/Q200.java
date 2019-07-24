60a4378c9d91289c2eea392cf7e7b8c7bc53bc7f
freshchen <961011595@qq.com>
2019-07-24 15:13:45 +0800

    [leetcode] leetcode

--- leetcode-java/src/main/java/Q200.java
+++ leetcode-java/src/main/java/Q200.java
@@ -6,23 +6,34 @@
  */
 public class Q200 {
     public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
         int result = 0;
         int row = grid.length;
         int column = grid[0].length;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
-                if (i - 1 < 0) grid[i - 1][j] = 0;
-                if (i + 1 == row) grid[i + 1][j] = 0;
-                if (j - 1 < 0) grid[i][j - 1] = 0;
-                if (j + 1 < column) grid[i][j + 1] = 0;
-                if ((grid[i][j] == 1) && (grid[i + 1][j] == 1) && (grid[i][j + 1] == 1)) {
-                    grid[i][j] = 0;
-                }
-                if ((grid[i][j] == 1) && (grid[i - 1][j] == 0) && (grid[i + 1][j] == 0) && (grid[i][j - 1] == 0) && (grid[i][j + 1] == 0)) {
+                if (grid[i][j] == 1) {
                     result++;
+                    infect(grid, i, j);
                 }
             }
         }
         return result;
     }
+
+    private void infect(char[][] grid, int i, int j) {
+        int row = grid.length;
+        int column = grid[0].length;
+
+        if (i < 0 || j < 0 || i >= row || j >= column || grid[i][j] == 0) {
+            return;
+        }
+        grid[i][j] = '0';
+        infect(grid, i - 1, j);
+        infect(grid, i + 1, j);
+        infect(grid, i, j - 1);
+        infect(grid, i, j + 1);
+
+    }
+
 }
--- leetcode-java/src/main/resources/leetcode-plugin/cn/[200]岛屿数量.java
+++ leetcode-java/src/main/resources/leetcode-plugin/cn/[200]岛屿数量.java
@@ -24,24 +24,38 @@
 //
 
 class Solution {
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
     public int numIslands(char[][] grid) {
-        int result = 0;
-        int row = grid.length;
-        int column = grid[0].length;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                if (i - 1 < 0) grid[i - 1][j] = 0;
-                if (i + 1 == row) grid[i + 1][j] = 0;
-                if (j - 1 < 0) grid[i][j - 1] = 0;
-                if (j + 1 < column) grid[i][j + 1] = 0;
-                if ((grid[i][j] == 1) && (grid[i + 1][j] == 1) && (grid[i][j + 1] == 1)) {
-                    grid[i][j] = 0;
-                }
-                if ((grid[i][j] == 1) && (grid[i - 1][j] == 0) && (grid[i + 1][j] == 0) && (grid[i][j - 1] == 0) && (grid[i][j + 1] == 0)) {
-                    result++;
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
                 }
             }
         }
-        return result;
+
+        return num_islands;
     }
 }
