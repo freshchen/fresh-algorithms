7830c2bcd2df52359e3036a2ccb5620af7c40ae0
freshchen <961011595@qq.com>
2019-09-08 23:07:41 +0800

    [fresh] practice

---
+++ practice-java/src/main/java/common/MyUtils.java
@@ -0,0 +1,14 @@
+package common;
+
+/**
+ * @program: fresh-algorithms
+ * @Date: 2019/9/8 23:02
+ * @Author: Ling Chen
+ * @Description:
+ */
+public class MyUtils {
+
+    public static void swap(){
+        System.out.println(1);
+    }
+}
